0e4d06b1a2b5a97dcbec575c396a292e0c7c0557
Pranav-Pra <68164506+Pranav-Pra@users.noreply.github.com>
2022-07-24 02:11:22 -0500

    exercise screen finished

--- app/src/main/java/com/example/rejuve/Dashboard.java
+++ app/src/main/java/com/example/rejuve/Dashboard.java
@@ -6,7 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Dashboard extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +21,21 @@ public class Dashboard extends AppCompatActivity {
     public void goToLeaderboard(View v) {
         Intent intent = new Intent(getApplicationContext(), Leaderboard.class);
         startActivity(intent);
+    }
+    public void goToExercise(View v) {
+        Intent intent = new Intent(getApplicationContext(), Exercise.class);
+        startActivity(intent);
+    }
+    public void goToNourishment(View v) {
+        Intent intent = new Intent(getApplicationContext(), Nourishment.class);
+        startActivity(intent);
+    }
+
+    public void signOut(View v) {
+        mAuth.getInstance().signOut();
+
+        Intent welcomeIntent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(welcomeIntent);
+
     }
 }
--- app/src/main/java/com/example/rejuve/Exercise.java
+++ app/src/main/java/com/example/rejuve/Exercise.java
@@ -1,14 +1,182 @@
 package com.example.rejuve;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Exercise extends AppCompatActivity {
+
+    CheckBox stretchCheck;
+    TextView stretch;
+    CheckBox jogCheck;
+    TextView jog;
+    CheckBox pushCheck;
+    TextView push;
+    CheckBox jumpCheck;
+    TextView jump;
+    CheckBox weightsCheck;
+    TextView weights;
+    CheckBox equipmentCheck;
+    TextView equipment;
+    CheckBox otherCheck;
+    TextView other;
+    private Integer numChecks;
+    private boolean isClickable;
+    private boolean nextScreenTimer;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
+
+        stretchCheck = findViewById(R.id.stretchCB);
+        jogCheck = findViewById(R.id.jogCB);
+        pushCheck = findViewById(R.id.pushCB);
+        jumpCheck = findViewById(R.id.jumpCB);
+        weightsCheck = findViewById(R.id.weightsCB);
+        equipmentCheck = findViewById(R.id.equipmentCB);
+        otherCheck = findViewById(R.id.otherCB);
+        numChecks = 0;
+        isClickable = false;
+        nextScreenTimer = false;
+
+        stretchCheck.setEnabled(true);
+        jogCheck.setEnabled(true);
+        pushCheck.setEnabled(true);
+        jumpCheck.setEnabled(true);
+        weightsCheck.setEnabled(true);
+        equipmentCheck.setEnabled(true);
+        otherCheck.setEnabled(true);
+
     }
+
+    public void ifStretchChecked(View v)
+    {
+        ifChecked();
+        if(isClickable) {
+            if (stretchCheck.isChecked()) {
+                numChecks++;
+            }
+        }
+    }
+    public void ifJogChecked(View v)
+    {
+        ifChecked();
+        if(isClickable) {
+            if (jogCheck.isChecked()) {
+                numChecks++;
+            }
+        }
+    }
+    public void ifPushChecked(View v)
+    {
+        ifChecked();
+        if(isClickable) {
+            if (pushCheck.isChecked()) {
+                numChecks++;
+            }
+        }
+    }
+    public void ifJumpChecked(View v)
+    {
+        ifChecked();
+        if(isClickable) {
+            if (jumpCheck.isChecked()) {
+                numChecks++;
+            }
+        }
+    }
+    public void ifWeightsChecked(View v)
+    {
+        ifChecked();
+        if(isClickable) {
+            if (weightsCheck.isChecked()) {
+                numChecks++;
+            }
+        }
+    }
+    public void ifEquipmentChecked(View v)
+    {
+        ifChecked();
+        if(isClickable) {
+            if (equipmentCheck.isChecked()) {
+                numChecks++;
+            }
+        }
+    }
+    public void ifOtherChecked(View v)
+    {
+        ifChecked();
+        if(isClickable) {
+            if (otherCheck.isChecked()) {
+                numChecks++;
+            }
+        }
+    }
+
+    public void ifChecked(){
+        if(numChecks < 3){
+            isClickable = true;
+        }
+        else{
+            Toast.makeText(Exercise.this, "Only 4 exercises can be selected", Toast.LENGTH_LONG).show();
+            isClickable = false;
+            stretchCheck.setEnabled(false);
+            jogCheck.setEnabled(false);
+            pushCheck.setEnabled(false);
+            jumpCheck.setEnabled(false);
+            weightsCheck.setEnabled(false);
+            equipmentCheck.setEnabled(false);
+            otherCheck.setEnabled(false);
+            nextScreenTimer = true;
+        }
+
+    }
+
+    public void reset(View v){
+        numChecks = 0;
+        isClickable = false;
+        nextScreenTimer = false;
+
+        stretchCheck.setChecked(false);
+        jogCheck.setChecked(false);
+        pushCheck.setChecked(false);
+        jumpCheck.setChecked(false);
+        weightsCheck.setChecked(false);
+        equipmentCheck.setChecked(false);
+        otherCheck.setChecked(false);
+
+        stretchCheck.setEnabled(true);
+        jogCheck.setEnabled(true);
+        pushCheck.setEnabled(true);
+        jumpCheck.setEnabled(true);
+        weightsCheck.setEnabled(true);
+        equipmentCheck.setEnabled(true);
+        otherCheck.setEnabled(true);
+    }
+
+    public void goToTimer(View v){
+        if(nextScreenTimer){
+            Intent intent = new Intent(getApplicationContext(), Timer.class);
+            startActivity(intent);
+        }
+        else{
+            Toast.makeText(Exercise.this, "Choose 4 exercises to move on", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+
+
+
+
+
 }
