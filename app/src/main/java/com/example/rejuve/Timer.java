782636a4ff37ea0f5b1c72e340ec9bbd3c046f03
Pranav-Pra <68164506+Pranav-Pra@users.noreply.github.com>
2022-07-24 17:12:36 -0500

    timer more

--- app/src/main/java/com/example/rejuve/Timer.java
+++ app/src/main/java/com/example/rejuve/Timer.java
@@ -2,12 +2,15 @@ package com.example.rejuve;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 //https://learntodroid.com/how-to-create-a-count-down-timer-in-android/
 
 public class Timer extends AppCompatActivity {
@@ -21,6 +24,11 @@ public class Timer extends AppCompatActivity {
 
     int counter;
     String exercise;
+    boolean exerciseOrReward = true;
+
+    LinearLayout timerLinearLayout;
+    LinearLayout buttonLinearLayout;
+    Button spinButton;
 
     String first;
     String second;
@@ -44,39 +52,46 @@ public class Timer extends AppCompatActivity {
     }
 
     private void setupButtons() {
-        start = findViewById(R.id.start_button);
-        end = findViewById(R.id.end_button);
-        start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                countDownText.setText(exercise);
-                startTime = 0;
-                startTime += Integer.parseInt("10") * 1000;
-                startTime += Integer.parseInt("00") * 60 * 1000;
-                start.setEnabled(false);
-                end.setEnabled(true);
-                timer = new CountDownTimer(startTime, 1000) {
-                    @Override
-                    public void onTick(long millisUntilFinished) {
-                        updateTimeRemaining(millisUntilFinished);
-                    }
 
+            start = findViewById(R.id.start_button);
+            end = findViewById(R.id.end_button);
+            if(exerciseOrReward) {
+                start.setOnClickListener(new View.OnClickListener() {
                     @Override
-                    public void onFinish() {
-                        finishTimer("Start Break");
-                        counter++;
-                    }
-                }.start();
-            }
-        });
+                    public void onClick(View v) {
+                        timerLinearLayout.setVisibility(View.VISIBLE);
 
-        end.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                timer.cancel();
-                finishTimer("Count down cancelled");
+                        countDownText.setText(exercise);
+                        startTime = 0;
+                        startTime += Integer.parseInt("10") * 1000;
+                        startTime += Integer.parseInt("00") * 60 * 1000;
+                        start.setEnabled(false);
+                        end.setEnabled(true);
+                        timer = new CountDownTimer(startTime, 1000) {
+                            @Override
+                            public void onTick(long millisUntilFinished) {
+                                updateTimeRemaining(millisUntilFinished);
+                            }
+
+                            @Override
+                            public void onFinish() {
+                                finishTimer("Break");
+                                timerLinearLayout.setVisibility(View.INVISIBLE);
+                                counter++;
+                            }
+                        }.start();
+                    }
+                });
+
+                end.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        timer.cancel();
+                        finishTimer("Count down cancelled");
+                    }
+                });
             }
-        });
+
     }
 
     @Override
@@ -97,8 +112,16 @@ public class Timer extends AppCompatActivity {
         secondsLeftText = findViewById(R.id.secondsLeftText);
         setupButtons();
 
+        timerLinearLayout = findViewById(R.id.timerLinearLayout);
+        timerLinearLayout.setVisibility(View.VISIBLE);
+        buttonLinearLayout = findViewById(R.id.buttonLinearLayout);
+        buttonLinearLayout.setVisibility(View.VISIBLE);
+        spinButton = findViewById(R.id.spinBtn);
+        spinButton.setVisibility(View.INVISIBLE);
+
         countDownText.setText(first);
         counter = 0;
+        exerciseOrReward = true;
 
     }
 
@@ -115,5 +138,17 @@ public class Timer extends AppCompatActivity {
         if(counter == 3){
             exercise = fourth;
         }
+        if(counter == 4){
+            exerciseOrReward = false;
+            buttonLinearLayout.setVisibility(View.INVISIBLE);
+            timerLinearLayout.setVisibility(View.INVISIBLE);
+            spinButton.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void goToReward(View v){
+
+            Intent intent = new Intent(getApplicationContext(), Reward.class);
+            startActivity(intent);
     }
 }
