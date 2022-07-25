d1cf8e7bedb93e25dd68c9196ed9894d7c349a9a
Pranav-Pra <68164506+Pranav-Pra@users.noreply.github.com>
2022-07-24 19:51:57 -0500

    quests ui

--- app/src/main/java/com/example/rejuve/GuildDashboard.java
+++ app/src/main/java/com/example/rejuve/GuildDashboard.java
@@ -12,6 +12,7 @@ import com.google.firebase.auth.FirebaseAuth;
 public class GuildDashboard extends AppCompatActivity {
     TextView guildCodeDisplay;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,4 +34,16 @@ public class GuildDashboard extends AppCompatActivity {
         startActivity(welcomeIntent);
 
     }
+
+    public void goToLeaderboard(View v) {
+        Intent intent = new Intent(getApplicationContext(), Leaderboard.class);
+        startActivity(intent);
+    }
+
+    public void goToQuests(View v) {
+        Intent intent = new Intent(getApplicationContext(), Challenges.class);
+        startActivity(intent);
+    }
+
+
 }
